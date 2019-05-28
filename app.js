d3af0350c7dd6b106e6ddcf37eb35509fabda726
jvitorn <41808876+jvitorn@users.noreply.github.com>
2019-05-27 22:04:16 -0300

    add in request form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //consts
+const http  = require('http');
 const express = require('express');
 const app  = express();
 const port = 3000;
@@ -9,9 +10,13 @@ const users = [
 //add folders
   app.use('/routes',express.static(__dirname + '/routes'));
   app.use('/views',express.static(__dirname + '/views'));
+//encode
+app.use(express.urlencoded());
 //index.
-app.get('/form',function(req,res){
-  res.send(req.body);
+app.post('/form',function(req,res){
+  var email = req.body.email;
+  console.log(email);
+  res.end();
 });
 //
 //server
