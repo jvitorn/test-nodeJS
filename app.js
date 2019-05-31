5f6fdfad00174b5ef13e185f2513dea1f82316bc
jvitorn <joaoeuno2011@gmail.com>
2019-05-31 20:13:39 -0300

    add login and route login

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const port = 3000;
   app.use('/routes',express.static(__dirname + '/routes'));
   app.use('/routes/stylesheets',express.static(__dirname + '/routes/stylesheets'));
   app.use('/routes/images',express.static(__dirname + '/routes/images'));
-  app.use('/routes/form',express.static(__dirname + '/routes/form.html'));
-  app.use('/routes/painel',express.static(__dirname + '/routes/painel.html');
+  app.use('/sigup',express.static(__dirname + '/routes/form.html'));
+  app.use('/painel',express.static(__dirname + '/routes/painel.html'));
+  app.use('/login',express.static(__dirname + '/routes/login.html'));
   app.use(express.static(__dirname + '/public'));
   app.use('/views',express.static(__dirname + '/views'));
 
