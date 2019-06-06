0f2103c5f0ae1003d621a1e14ec7fd36702b8aca
jvitorn <joaoeuno2011@gmail.com>
2019-06-05 22:01:54 -0300

    Add Painel and Add front-end public page painel

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const path = require('path');
 
 //add folders
   app.use('/routes',express.static(__dirname + '/routes'));
-  app.use('/routes/stylesheets',express.static(__dirname + '/routes/stylesheets'));
-  app.use('/routes/images',express.static(__dirname + '/routes/images'));
+  app.use('/stylesheets',express.static(__dirname + '/public/stylesheets'));
+  app.use('/images',express.static(__dirname + '/public/images'));
   app.use('/sigup',express.static(__dirname + '/routes/form.html'));
-  app.use('/painel',express.static(__dirname + '/routes/painel.html'));
+  app.use('/painel',express.static(__dirname + '/public/painel.html'));
   app.use('/login',express.static(__dirname + '/routes/login.html'));
   app.use(express.static(__dirname + '/public'));
   app.use('/views',express.static(__dirname + '/views'));
